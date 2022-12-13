6fe04314e12932a7c2886c8e7f45fc3da2230773
Pedro Augusto <ppedroadas@gmail.com>
2022-12-13 14:50:44 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+var cont = 0;
+
 function clickMenu(){
-    bag.style.visibility = 'hidden';
-    header.style.backgroundColor = 'red';
+
+    if(cont == 0){
+        bag.style.visibility = 'hidden';
+        document.getElementById('test').style.backgroundColor = 'black';
+        document.getElementById(`nav`).style.backgroundColor = 'black';
+        document.getElementById('test').style.height = '100vh';
+        document.getElementById('menuMobil').style.display = 'block'
+        cont++;
+    }else{
+        bag.style.visibility = '';
+        document.getElementById('test').style.backgroundColor = 'black';
+        document.getElementById(`nav`).style.backgroundColor = 'var(--colorTitle)';
+        document.getElementById('test').style.height = '48px';
+        document.getElementById('menuMobil').style.display = 'none'
+        cont = 0;
+    }
 }
