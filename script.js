56868c8a2d2d29f617c271767fb26f90e8725bec
Pedro Augusto <ppedroadas@gmail.com>
2022-12-15 15:58:04 -0300

    update

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
+
 var cont = 0;
-var main = document.getElementById('contents')
+
+function onMenuMobil(){
+    document.getElementById('bag').style.visibility = 'hidden';
+    document.getElementById('test').style.backgroundColor = 'black';
+    document.getElementById('test').style.opacity = '1';
+    document.getElementById(`nav`).style.backgroundColor = 'black';
+    document.getElementById('test').style.height = '100vh';
+    document.getElementById('menuMobil').style.display = 'block'
+    document.getElementById('contents').style.display = '';
+    cont++; 
+}
+
+function offMenuMobil(){
+    bag.style.visibility = '';
+    document.getElementById('nav').style.backgroundColor = 'var(--colorTitle)';
+    document.getElementById('test').style.backgroundColor = 'var(--colorTitle)';
+    document.getElementById('test').style.height = '48px';
+    document.getElementById('menuMobil').style.display = 'none'
+    document.getElementById('contents').style.display = 'block';
+    cont = 0;
+}
+
+function adjustingMenuMobil(){
+    if (document.body.clientWidth > 765) {
+        return offMenuMobil();
+    }
+}
 
 function clickMenu(){
 
         if(cont == 0){
-            bag.style.visibility = 'hidden';
-            document.getElementById('test').style.backgroundColor = 'black';
-            document.getElementById('test').style.opacity = '1';
-            document.getElementById(`nav`).style.backgroundColor = 'black';
-            document.getElementById('test').style.height = '100vh';
-            document.getElementById('menuMobil').style.display = 'block'
-            main.style.display = 'none';
-            cont++; 
-
+            return onMenuMobil();
         }else{
-            bag.style.visibility = '';
-            document.getElementById('test').style.backgroundColor = 'black';
-            document.getElementById(`nav`).style.backgroundColor = 'var(--colorTitle)';
-            document.getElementById('test').style.height = '48px';
-            document.getElementById('menuMobil').style.display = 'none'
-            document.getElementById('contents').style.display = 'block';
-            cont = 0;
+            return offMenuMobil();
     }
 }
 
@@ -28,9 +41,7 @@ function clickInputSear(){
     document.getElementById('iSearch').style.width = '72.5%'
     document.getElementById('cancel').style.display = 'inline-block'
     document.getElementById(`nav`).style.display = 'none';
-    document.getElementById(`input`).style.marginTop = '15px';
-
-    
+    document.getElementById(`input`).style.marginTop = '15px';   
 }
 
 function clickCancel(){
